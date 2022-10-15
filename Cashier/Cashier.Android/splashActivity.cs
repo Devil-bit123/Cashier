e9896b619021dc1f63236c2a6f7ff172753b4c27
Devil-bit123 <80175560+Devil-bit123@users.noreply.github.com>
2022-10-15 15:03:17 -0500

    Agregar archivos de proyecto.

---
+++ Cashier/Cashier.Android/splashActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cashier.Droid
+{
+    
+    [Activity(Label = "Cashier", Icon = "@mipmap/icon",
+        Theme = "@style/splashTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize )]
+    public class splashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            // Create your application here
+        }
+    }
+}
